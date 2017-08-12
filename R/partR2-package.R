c5ceffd2bef748213539c6a7dcd7a72580442829
Martin Stoffel <martin.adam.stoffel@gmail.com>
2017-08-12 20:07:16 +0200

    travis added

---
+++ R/partR2-package.R
@@ -0,0 +1,14 @@
+#' R2, commonality coefficients and structure coefficients for mixed models
+#'
+#'
+#' @references
+#'
+#' Nakagawa, S., & Schielzeth, H. (2013). \emph{A general and simple method for obtaining R2 from
+#' generalized linear mixed‐effects models}. Methods in Ecology and Evolution, 4(2), 133-142.
+#'
+#' Newton, R. G., & Spurrell, D. J. (1967).  \emph{A development of multiple regression for the
+#' analysis of routine data. Applied Statistics}. 51-64.
+#'
+#' @docType package
+#' @name partR2
+NULL
