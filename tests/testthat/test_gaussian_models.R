738f2d8a501eb8161be18b256c812eb986c76f12
Martin Stoffel <martin.adam.stoffel@gmail.com>
2020-02-10 09:51:37 +0000

    new biomass dataset with fewer warnings

--- tests/testthat/test_gaussian_models.R
+++ tests/testthat/test_gaussian_models.R
@@ -21,22 +21,22 @@ r2_mod1_6 <- partR2(mod1, data = biomass, partvars = c("Temperature*Precipitatio
                                        "Year", "SpeciesDiversity"))
 
 future::plan(future::multisession, workers = 3)
-r2_mod1_7 <-  partR2(mod1, data = biomass, nboot = 5000, parallel = TRUE)
+r2_mod1_7 <-  partR2(mod1, data = biomass, nboot = 10, parallel = TRUE)
 
 #r2_mod1_6 <- partR2(mod1, partvars = c("Temperature*Precipitation"))
 test_that("Gaussian models with increasing complexity give correct R2s", {
     # pe
-    expect_equal(r2_mod1_1$R2_pe_ci$R2, 0.460, tolerance = 0.01)
+    expect_equal(r2_mod1_1$R2_pe_ci$R2, 0.62, tolerance = 0.01)
     # ci
-    expect_equal(r2_mod1_2$R2_pe_ci$CI_lower, 0.402, tolerance = 0.01)
-    expect_equal(r2_mod1_2$R2_pe_ci$CI_upper, 0.567, tolerance = 0.01)
+    expect_equal(r2_mod1_2$R2_pe_ci$CI_lower, 0.534, tolerance = 0.01)
+    expect_equal(r2_mod1_2$R2_pe_ci$CI_upper, 0.689, tolerance = 0.01)
     # R2 one partvar
-    expect_equal(r2_mod1_3$R2_pe_ci$R2, c(0.460, 0.051), tolerance = 0.01)
+    expect_equal(r2_mod1_3$R2_pe_ci$R2, c(0.62,  0.177), tolerance = 0.01)
     # R2 two partvars
-    expect_equal(r2_mod1_4$R2_pe_ci$R2, c(0.460, 0.051, 0.032, 0.084),
+    expect_equal(r2_mod1_4$R2_pe_ci$R2, c(0.62,  0.177, 0.009, 0.187),
                  tolerance = 0.01)
     # interaction only
-    expect_equal(r2_mod1_5$R2_pe_ci$R2, c(0.460, 0.013), tolerance = 0.01)
+    expect_equal(r2_mod1_5$R2_pe_ci$R2, c(0.62, 0.019), tolerance = 0.01)
     # all together same as marginal
     expect_equal(r2_mod1_6$R2_pe_ci$R2[length(r2_mod1_6$R2_pe_ci$R2)],
                  r2_mod1_6$R2_pe_ci$R2[1], tolerance = 0.001)
@@ -49,11 +49,11 @@ mod2 <- suppressWarnings(lmer(Biomass ~ Year + I(Temperature^2) * Precipitation 
              data = biomass))
 
 r2_mod2_1 <- partR2(mod2, data = biomass)
-r2_mod2_2 <- partR2(mod2, data = biomass, partvars = c("Year", "Precipitation",
+r2_mod2_2 <-  suppressWarnings(partR2(mod2, data = biomass, partvars = c("Year", "Precipitation",
                                                           "I(Temperature^2)",
-                                                          "I(Temperature^2):Precipitation", "SpeciesDiversity"))
+                                                          "I(Temperature^2):Precipitation", "SpeciesDiversity")))
 test_that("Predictor manipulation in formula works", {
-    expect_equal(r2_mod2_1$R2_pe_ci$R2, 0.419, tolerance = 0.01)
+    expect_equal(r2_mod2_1$R2_pe_ci$R2, 0.595, tolerance = 0.01)
     # all together same as marginal
     expect_equal(r2_mod2_2$R2_pe_ci$R2[length(r2_mod2_2$R2_pe_ci$R2)],
                  r2_mod2_2$R2_pe_ci$R2[1], tolerance = 0.001)
