c6317c1e833c0566c94f56901763685a25cc8449
Martin Stoffel <martin.adam.stoffel@gmail.com>
2020-03-10 09:16:27 +0000

    grasshoppers added

--- R/datasets.R
+++ R/datasets.R
@@ -13,6 +13,30 @@
 #' @name biomass
 NULL
 
+#' Grasshoppers dataset
+#'
+#' @details This is a real dataset from grasshoppers.
+#'
+#' This dataset contains data on spatial variation in color morph ratios in a
+#' color-polymorphic species of grasshopper (Dieker et al 2018). Individuals of this species occur
+#' either in green or a brown color variant and the dataset contains counts of
+#' brown and green individuals (seprarated for females and males) from 42 sites
+#' sampled in the field. All 'Bio' variables describe various aspects of ecologically
+#' relevant climatic conditions (see Karger et al. 2017).
+#'
+#' @references
+#' Dieker, P., L. Beckmann, J. Teckentrup, and H. Schielzeth (2018)
+#' \emph{Spatial analyses of two colour polymorphisms in an alpine grasshopper reveal
+#' a role of small-scale heterogeneity}. Ecology and Evolution, 8, 7273-7284.
+#'
+#' Karger, D. N., O. Conrad, J. Bohner, T. Kawohl, H. Kreft, R. W. Soria-Auza,
+#' N. E. Zimmermann, H. P. Linder, and M. Kessler (2017) \emph{Data descriptor: Climatologies
+#' at high resolution for the earth's land surface areas}. Scientific Data, 4, 170122.
+#'
+#' @keywords datasets
+#' @name Grasshoppers
+NULL
+
 #' GuineaPigs dataset
 #'
 #' @details This is a real dataset from guinea pigs.
