c49a57a34b41f5bb882c213571266cec5afb265a
Martin Stoffel <martin.adam.stoffel@gmail.com>
2020-12-28 11:57:09 +0000

    tests for R2 and part R2 subfunctions

--- tests/testthat/test_r2.R
+++ tests/testthat/test_r2.R
@@ -1,4 +1,4 @@
-context("part R2 calculation")
+context("Calculating R2 and part R2")
 
 data(biomass)
 biomass[] <- lapply(biomass, function(x) if (is.double(x)) scale(x) else x)
@@ -7,7 +7,9 @@ fit1 <- lme4::lmer(Biomass ~  SpeciesDiversity + I(Temperature^2) + (1 | Populat
 fit2 <- lme4::lmer(Biomass ~  Year + SpeciesDiversity * I(Temperature^2) + (1 | Population),
                    data = biomass)
 
+
 # R2 point estimate
+
 r2_marg <- R2_pe(fit1, expct = "meanobs", overdisp_name = "overdisp",
                  R2_type = "marginal")
 r2_cond <- R2_pe(fit1, expct = "meanobs", overdisp_name = "overdisp",
@@ -68,3 +70,39 @@ test_that("Reduced model R2s are correct in R2_of_red_mod", {
     expect_equal(r2_5a, r2_5b)
 })
 
+
+
+# part R2s
+all_combs1 <- make_combs(partvars =  c("Year"),
+                         partbatch = NULL, max_level = NULL)
+all_combs2 <- make_combs(partvars =  c("Year", "SpeciesDiversity"),
+                         partbatch = NULL, max_level = NULL)
+all_combs3 <- make_combs(partvars =  c("Year", "SpeciesDiversity",
+                                      "I(Temperature^2)",
+                                      "SpeciesDiversity:I(Temperature^2)"),
+                        partbatch = NULL, max_level = NULL)
+
+r2_6 <- part_R2s(fit2, expct = "meanobs", overdisp_name = "overdisp",
+                  R2_type = "marginal", data_mod = biomass, partition = TRUE,
+                  allow_neg_r2 = FALSE, all_comb = all_combs1)
+r2_7 <- part_R2s(fit2, expct = "meanobs", overdisp_name = "overdisp",
+                 R2_type = "marginal", data_mod = biomass, partition = TRUE,
+                 allow_neg_r2 = FALSE, all_comb = all_combs2)
+r2_8 <- part_R2s(fit2, expct = "meanobs", overdisp_name = "overdisp",
+                 R2_type = "marginal", data_mod = biomass, partition = TRUE,
+                 allow_neg_r2 = FALSE, all_comb = all_combs3)
+
+test_that("part R2s are correct with part_R2s()", {
+
+    expect_equal(r2_6$term, c("Full", "Year"))
+    expect_equal(r2_6$estimate, c(0.219, 0.0200), tolerance = 0.001)
+
+    expect_equal(nrow(r2_7), 4)
+    expect_equal(sum(r2_7$term %in% "Year+SpeciesDiversity"), 1)
+
+    # some random checks
+    expect_equal(nrow(r2_8), 16)
+    expect_equal(sum(r2_8$term %in% "SpeciesDiversity:I(Temperature^2)"), 1)
+    expect_equal(r2_8[r2_8$term == "Year+SpeciesDiversity:I(Temperature^2)", "estimate"][[1]], 0.0195, tolerance = 0.0001)
+
+})
